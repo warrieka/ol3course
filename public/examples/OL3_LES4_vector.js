3f7fa4cf2bc24d3986d32f5a8a1a3cc2c34ebdf9
warrieka <kaywarrie@gmail.com>
2015-02-23 09:08:45 +0100

    fix

--- public/examples/OL3_LES4_vector.js
+++ public/examples/OL3_LES4_vector.js
@@ -1,8 +1,8 @@
-var vectorLayer;
+
 $( document ).ready(function() {
 
 var vectorSource  = new ol.source.GeoJSON({
-    projection:'EPSG:31370',
+    projection: 'EPSG:3857',
     defaultProjection: 'EPSG:4326',
     url: "/resource/zendmast.geojson"
 });
@@ -17,47 +17,25 @@ var iconStyle = new ol.style.Style({
     })
  });
 
-vectorLayer = new ol.layer.Vector({
+var vectorLayer = new ol.layer.Vector({
     source: vectorSource,
     style: iconStyle
 });
 
-var projectionExtent = [9928.00, 66928.00, 272072.00, 329072.00];
-var projection = ol.proj.get('EPSG:31370');
-projection.setExtent(projectionExtent);
-var size = ol.extent.getWidth(projectionExtent) / 256;
-var resolutions = new Array(16);
-var matrixIds = new Array(16);
-for (var z = 0; z < 16; ++z) {
-    resolutions[z] = size / Math.pow(2, z);
-    matrixIds[z] = z;
-}
-
 var grb = new ol.layer.Tile({
-    extent: projectionExtent,
-    source: new ol.source.WMTS({
-    url: 'http://grb.agiv.be/geodiensten/raadpleegdiensten/geocache/wmts/',
-    layer: 'orthoklm',
-    matrixSet: 'BPL72VL',
-    format: 'image/png',
-    projection: projection,
-    tileGrid: new ol.tilegrid.WMTS({
-        origin: ol.extent.getTopLeft(projectionExtent),
-        resolutions: resolutions,
-        matrixIds: matrixIds
-        }),
-    style: 'default'
-    })
-});
-        
+    source: new ol.source.XYZ({
+        url: 'http://grb.agiv.be/geodiensten/raadpleegdiensten/geocache/tms/1.0.0/grb_bsk@GoogleMapsVL/{z}/{x}/{-y}.png'
+    })    
+}); 
+
 var map = new ol.Map({
         target: 'map',
         layers: [ grb , vectorLayer],
         view: new ol.View({
-            projection: 'EPSG:31370',
-            center: ol.proj.transform([4,51], 'EPSG:4326', 'EPSG:31370'),
-            zoom: 3,
-            maxZoom: 17
+            projection: 'EPSG:3857',
+            center: ol.proj.transform([4,51], 'EPSG:4326', 'EPSG:3857'),
+            zoom: 12,
+            maxZoom: 21
         })
     });
 
